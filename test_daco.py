8553cb9d3a5df00c564fe024953e134f6c319954
Jon Vegard Sparre <jonvegards@gmail.com>
2018-12-14 15:55:01 +0100

    nytt datasett for testing

--- test_daco.py
+++ test_daco.py
@@ -2,38 +2,24 @@ from daco import daco
 
 import pandas as pd
 
+header = ['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race','sex','capital-gain','capital-loss','hours-per-week','native-country', 'pred_var']
+# 39, State-gov, 77516, Bachelors, 13, Never-married, Adm-clerical, Not-in-family, White, Male, 2174, 0, 40, United-States, <=50K
+df = pd.read_csv('~/github/ai-lab-daco/datasets/adult.data.txt', sep=",",names=header, header=None)
 
-header = [
-          "A1 - Checking account" # status of exisiting checking account
-         ,"A2 - Duration(month)" # duration in month
-         ,"A3 - Credit history" #credit history
-         ,"A4 - Purpose" # what is the loan for?
-         ,"A5 - Credit amount" # how much is asked for?
-         ,"A6 - Savings account" #status of savings account/bonds
-         ,"A7 - Employment duration" # present employment since
-         ,"A8 - Installment rate" # installment rate in percentage of disposable income
-         ,"A9 - Civil status and gender" # personal status and gender
-         ,"A10 - Debtors/guarantors" # other debtors / guarantors?
-         ,"A11 - Present residence since" # present residence since?
-         ,"A12 - Property" # property
-         ,"A13 - Age" # age in years
-         ,"A14 - Other installment plans" # other installment plans?
-         ,"A15 - Housing" # Housing: rent, own, for free?
-         ,"A16 - Existing credits" # number of existing credits at this bank
-         ,"A17 - Job" # unemployed? unskilled? self-employed? highly qualified employee? etc.
-         ,"A18 - Liability" # number of people liable to provie maintenance for
-         ,"A19 - Telephone" # has a phone?
-         ,"A20 - Foreign worker" # is a foreign worker?
-         ,"Score" # 1 = Good, 2 = Bad
-        ]
+cat_var = ['workclass','education','marital-status','occupation','relationship','race','sex','native-country', 'pred_var']
+for var in (set(header) - set(cat_var)):
+  df[var] = df[var].astype('float')
 
-df1 = pd.read_csv('~/github/ai-lab-daco/datasets/xaa-numeric.csv', sep="\s+",names=header, header=None)
-df2 = pd.read_csv('~/github/ai-lab-daco/datasets/xab-numeric.csv', sep="\s+",names=header, header=None)
+for var in cat_var:
+  df[var] = df[var].astype('category')
 
-daco_obj = daco(df1[header[-20:]], df2[header[-20:]])
-dist = daco_obj.findDistributions()
+print(df.info())
 
-D, p = daco_obj.chisquare('A6 - Savings account')
+df1 = df[:15000]
+df2 = df[15000:].drop(columns='age')
 
-# dict with D and p values now available as attribute:
-daco_obj.p_D_chisquare
+daco_obj = daco(df1,df2)
+# dist = daco_obj.findDistributions()
+
+# print(daco_obj.ks2_test('age'))
+# print(daco_obj.ks2_test_val)
