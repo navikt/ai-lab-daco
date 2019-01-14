d9f8b51b09ac9867a30d2c9faabb212cc503fe7c
Jon Vegard Sparre <jonvegards@gmail.com>
2019-01-14 14:16:59 +0100

    daco-pakka er klar

--- daco/setup.py
+++ daco/setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="daco",
     version="0.0.1",
@@ -10,10 +7,24 @@ setuptools.setup(
     author_email="jon.vegard.sparre@nav.no",
     description="A package for comparing two datasets",
     url="https://navikt.github.io/ai-lab-daco/",
-    packages=setuptools.find_packages(),
+    packages='daco',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'cycler==0.10.0',
+        'daco==0.0.1',
+        'kiwisolver==1.0.1',
+        'matplotlib==3.0.2',
+        'numpy==1.16.0',
+        'pandas==0.23.4',
+        'pyparsing==2.3.1',
+        'python-dateutil==2.7.5',
+        'pytz==2018.9',
+        'scipy==1.2.0',
+        'seaborn==0.9.0',
+        'six==1.12.0',
+    ]
 )
